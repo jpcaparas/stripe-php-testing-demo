88159765e7f40909f6d162fa447c8a785ceac5f9
JP Caparas <jp@jpcaparas.com>
2018-05-03 23:16:20 +1200

    Add phpunit library and bootstrap config

---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HelloWorldTest extends TestCase
+{
+    public function testHelloWorld()
+    {
+        $this->assertEquals('Hello World', 'Hello World');
+    }
+}
